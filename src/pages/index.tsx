9b191e05aedbd644d4fb419b51992d60dd89013a
Eugene Petryankin <epetryankin@devvela.com>
2025-01-17 19:50:46 +0300

    Add tanstack query

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,100 @@
 import Head from "next/head";
-import { useEffect, useState } from "react";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { zodResolver } from "@hookform/resolvers/zod";
 import {
   todoSchema,
   addTodoFormSchema,
   voidResponseSchema,
   type AddTodoForm,
-  type Todo,
 } from "@/models";
 import { XCircleIcon } from "@heroicons/react/16/solid";
 import { Inter } from "next/font/google";
 import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const [todos, setTodos] = useState<Todo[]>([]);
+  const queryClient = useQueryClient();
+
+  const { data: todos, isPending } = useQuery({
+    queryKey: ["todos"],
+    queryFn: async () => {
+      const res = await fetch("/api/todos");
+      const data = todoSchema.array().parse(await res.json());
+      return data;
+    },
+  });
+
+  const addTodo = useMutation({
+    mutationFn: async (text: string) => {
+      const res = await fetch("/api/todos", {
+        method: "POST",
+        body: JSON.stringify({ text }),
+      });
+
+      const data = voidResponseSchema.parse(await res.json());
+
+      if (!data.success) {
+        throw new Error("Something went wrong");
+      }
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["todos"] });
+      reset();
+    },
+    onError: (error) => {
+      console.error(error);
+    },
+  });
+
+  const toggleIsTodoCompleted = useMutation({
+    mutationFn: async (id: string) => {
+      if (!todos?.length) return;
+
+      const todo = todos.find((t) => t.id === id)!;
+
+      const res = await fetch("/api/todos", {
+        method: "PUT",
+        body: JSON.stringify({
+          ...todo,
+          isCompleted: !todo.isCompleted,
+        }),
+      });
+
+      const data = voidResponseSchema.parse(await res.json());
+
+      if (!data.success) {
+        throw new Error("Something went wrong");
+      }
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["todos"] });
+    },
+    onError: (error) => {
+      console.error(error);
+    },
+  });
+
+  const deleteTodo = useMutation({
+    mutationFn: async (id: string) => {
+      const res = await fetch("/api/todos", {
+        method: "DELETE",
+        body: JSON.stringify({ id }),
+      });
+
+      const data = voidResponseSchema.parse(await res.json());
+
+      if (!data.success) {
+        throw new Error("Something went wrong");
+      }
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["todos"] });
+    },
+    onError: (error) => {
+      console.error(error);
+    },
+  });
 
   const {
     register,
@@ -26,66 +105,9 @@ export default function Home() {
     resolver: zodResolver(addTodoFormSchema),
   });
 
-  useEffect(() => {
-    fetchTodos();
-  }, []);
-
-  const fetchTodos = async () => {
-    const res = await fetch("/api/todos");
-    const data = todoSchema.array().parse(await res.json());
-    setTodos(data);
-  };
-
-  const addTodo = async (text: string) => {
-    const res = await fetch("/api/todos", {
-      method: "POST",
-      body: JSON.stringify({ text }),
-    });
-
-    const data = voidResponseSchema.parse(await res.json());
-
-    if (!data.success) {
-      console.error("Something went wrong");
-    }
-
-    reset();
-    fetchTodos();
-  };
-
-  const toggleIsTodoCompleted = async (id: string) => {
-    const todo = todos.find((t) => t.id === id)!;
-
-    const res = await fetch("/api/todos", {
-      method: "PUT",
-      body: JSON.stringify({
-        ...todo,
-        isCompleted: !todo.isCompleted,
-      }),
-    });
-
-    const data = voidResponseSchema.parse(await res.json());
-
-    if (!data.success) {
-      console.error("Something went wrong");
-    }
-
-    fetchTodos();
-  };
-
-  const deleteTodo = async (id: string) => {
-    const res = await fetch("/api/todos", {
-      method: "DELETE",
-      body: JSON.stringify({ id }),
-    });
-
-    const data = voidResponseSchema.parse(await res.json());
-
-    if (!data.success) {
-      console.error("Something went wrong");
-    }
-
-    fetchTodos();
-  };
+  if (isPending || !todos) {
+    return null;
+  }
 
   return (
     <div className={`${inter.className} flex flex-col`}>
@@ -97,7 +119,7 @@ export default function Home() {
 
       <form
         onSubmit={handleSubmit(({ text }) => {
-          addTodo(text);
+          addTodo.mutate(text);
         })}
         className="flex max-w-xs items-start gap-4 px-4 pb-6 pt-2"
       >
@@ -120,14 +142,14 @@ export default function Home() {
               <input
                 type="checkbox"
                 checked={todo.isCompleted}
-                onChange={() => toggleIsTodoCompleted(todo.id)}
+                onChange={() => toggleIsTodoCompleted.mutate(todo.id)}
                 className="checked:border-green-600 checked:bg-green-600 checked:hover:border-green-700 checked:hover:bg-green-700"
               />
               <p>{todo.text}</p>
             </label>
 
             <button
-              onClick={() => deleteTodo(todo.id)}
+              onClick={() => deleteTodo.mutate(todo.id)}
               className="text-red-600"
             >
               <span className="sr-only">Delete todo</span>
